8d4b93fdb9fc8f2b083207692647eabcc89aa75d
无束 <lijing.pt@alibaba-inc.com>
2020-06-17 09:37:25 +0800

    状态栏与背景融合..

--- app/src/main/java/com/coolweahter/android/WeatherActivity.java
+++ app/src/main/java/com/coolweahter/android/WeatherActivity.java
@@ -9,6 +9,8 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Gravity;
@@ -109,6 +111,12 @@ public class WeatherActivity extends AppCompatActivity {
                 requestWeather(mWeatherId);
             }
         });
+
+        if(Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor((Color.TRANSPARENT));
+        }
 
     }
 
